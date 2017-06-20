b4698fe68b57d9e207cc07bd96e1793904bc2742
Cristian Romanescu <cristian@romanescu.ro>
2017-06-20 11:43:23 +0300

    Grey out aichi targets

--- docroot/sites/all/themes/informea.theme/js/goals.js
+++ docroot/sites/all/themes/informea.theme/js/goals.js
@@ -6,6 +6,11 @@
         $(this).on('click', function() { return false; });
         $(this).attr('href', '#');
       }
+      if (this.text === 'Aichi Targets') {
+        $(this).css({ 'color' : '#c0c0c0' });
+        $(this).on('click', function() { return false; });
+        $(this).attr('href', '#');
+      }
     });
   })
 })(jQuery);
