fd1259034b5d8920332c8218e064335a226e568d
Cristian Romanescu <cristian@romanescu.ro>
2017-06-20 11:19:46 +0300

    Last minute changes to goals page

---
+++ docroot/sites/all/themes/informea.theme/js/goals.js
@@ -0,0 +1,11 @@
+(function ($) {
+  $(document).ready(function() {
+    $('h2 a').each(function() {
+      if (this.text === 'GEG') {
+        $(this).css({ 'color' : '#c0c0c0' });
+        $(this).on('click', function() { return false; });
+        $(this).attr('href', '#');
+      }
+    });
+  })
+})(jQuery);
