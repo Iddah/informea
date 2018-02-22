ed63250b3673bc16cd21fa878d8642090707b1cc
marapredescu1 <mara.predescu@eaudeweb.ro>
2018-02-22 17:25:50 +0200

    Issue #342 facets ranged slider wip

--- docroot/sites/all/modules/informea/search_api_ion_range_slider/search-api-ion-range-slider-block-slider-view-form.tpl.php
+++ docroot/sites/all/modules/informea/search_api_ion_range_slider/search-api-ion-range-slider-block-slider-view-form.tpl.php
@@ -6,11 +6,11 @@
 <?php print drupal_render($form['text-range']); ?>
 <h3>XD</h3>
 <div class="yui3-g">
-  <div class="yui3-u range-box range-box-left">
-    <?php print drupal_render($form['range-from']); ?>
-  </div>
   <div class="yui3-u range-slider-box">
     <?php print drupal_render($form['range-slider']); ?>
+  </div>
+  <div class="yui3-u range-box range-box-left">
+    <?php print drupal_render($form['range-from']); ?>
   </div>
   <div class="yui3-u range-box range-box-right">
     <?php print drupal_render($form['range-to']); ?>
