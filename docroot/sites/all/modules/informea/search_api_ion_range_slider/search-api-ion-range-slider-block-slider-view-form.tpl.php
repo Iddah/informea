afc8898920e45750cb1e614692f70d0f0fc33cdf
marapredescu1 <mara.predescu@eaudeweb.ro>
2018-02-23 13:39:17 +0200

    Issue #342 custom styling for ion range slider

--- docroot/sites/all/modules/informea/search_api_ion_range_slider/search-api-ion-range-slider-block-slider-view-form.tpl.php
+++ docroot/sites/all/modules/informea/search_api_ion_range_slider/search-api-ion-range-slider-block-slider-view-form.tpl.php
@@ -3,20 +3,22 @@
  * @file custom Search API ranges Min/Max UI slider widget
  */
 ?>
-<?php print drupal_render($form['text-range']); ?>
-<div class="range-slider-box">
-  <?php print drupal_render($form['range-slider']); ?>
-</div>
-<div class="ion-rangeslider">
-  <div class="ion-rangeslider__left range-box range-box-left">
-    <?php print drupal_render($form['range-from']); ?>
+<div class="ion-range-slider">
+  <?php print drupal_render($form['text-range']); ?>
+  <div class="ion-range-slider-box">
+    <?php print drupal_render($form['range-slider']); ?>
   </div>
-  <div class="ion-rangeslider__right range-box range-box-right">
-    <?php print drupal_render($form['range-to']); ?>
+  <div class="ion-range-slider-form">
+    <div class="ion-range-box ion-range-box-left">
+      <?php print drupal_render($form['range-from']); ?>
+    </div>
+    <div class="ion-range-box ion-range-box-right">
+      <?php print drupal_render($form['range-to']); ?>
+    </div>
   </div>
+  <?php print drupal_render($form['submit']); ?>
+  <?php
+    // Render required hidden fields.
+    print drupal_render_children($form);
+  ?>
 </div>
-<?php print drupal_render($form['submit']); ?>
-<?php
-  // Render required hidden fields.
-  print drupal_render_children($form);
-?>
