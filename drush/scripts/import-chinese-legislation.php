39bdb9a94be3f68eaef2fadc5f0a4e36440e6e50
Eau de Web <iddahkamau@gmail.com>
2018-03-15 10:52:38 +0200

    case laws bulk upload script and data

--- drush/scripts/import-chinese-legislation.php
+++ drush/scripts/import-chinese-legislation.php
@@ -11,23 +11,36 @@ class LegislationImportFromCSV {
     if (($handle = fopen($csvFile, "r")) !== FALSE) {
       $i = 0;
       while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        // Ignore first line - headeer
+        // Ignore first line - header
         if ($i > 0) {
           $row = [];
-          $row['title_en'] = $data[1];
-          // @TODO Copy all CSV columns to the array.
+          $row['title_field_en'] = trim($data[2]);
+          $row['title_field_cn'] = trim($data[1]);
+          $row['field_date_of_original_text'] = trim($data[3]);
+          $row['field_sorting_date'] = trim($data[4]);
+          $row['field_type_of_text'] = trim($data[5]);
+          $row['field_informea_tags'] = trim($data[6]);
+          $row['field_url_cn'] = $data[7];
+          $row['field_url_en'] = $data[8];
           $this->records[] = $row;
         }
         $i++;
       }
       fclose($handle);
     }
-    drush_log(count($this->records) . " records loaded from the CSV file ...\n", Drush\Log\LogLevel::OK);
+    drush_log(
+      sprintf(
+        "%d records loaded from the CSV file: %s\n", 
+        count($this->records), 
+        realpath($csvFile)
+      ), 
+      Drush\Log\LogLevel::OK);
   }
 
 
   public function import() {
     foreach($this->records as $idx => $record) {
+      //var_dump($record);
       try {
         $node = $this->createNode($record);
         // TODO
@@ -35,6 +48,9 @@ class LegislationImportFromCSV {
         // 2. Add other chinese fields, like title_field['zh-cn'][0]['value'] = 'chinese title'
         // 3. Save node
         // 3. Mark chinese translation available - see setTranslation below
+       // var_dump ($url);
+        node_save($node);
+        $this->setTranslation($node, 'zh-hans');
         node_save($node);
       } catch (Exception $e) {
         drush_log(
@@ -51,9 +67,34 @@ class LegislationImportFromCSV {
    * Create a new node object and return it.
    */
   public function createNode($record, $type = 'legislation', $language = 'en') {
+
+    $vdk = taxonomy_vocabulary_machine_name_load('type_of_text');
+    $type_of_text_vid = $vdk->vid;
+    $vdk = taxonomy_vocabulary_machine_name_load('thesaurus_informea');
+    $thesaurus_vid = $vdk->vid;
+
+    $type_of_text_tid = ODataMigrationUtil::getTaxonomyTermByName($record['field_type_of_text'], $type_of_text_vid);
+    if (empty($type_of_text_tid)) {
+      throw new Exception('Cannot find type of text = ' . $record['field_type_of_text']);
+    }
     $node = new stdClass();
-    $node->title = $record['title_en'];
-    // TODO add all fields.
+    $node->title = $record['title_field_en'];
+    $node->title_field['en'][0]['value'] = $node->title;
+    $node->title_field['zh-hans'][0]['value'] = $record['title_field_cn'];
+    $node->field_date_of_original_text[LANGUAGE_NONE][0]['value'] = $record['field_date_of_original_text'];
+    $node->field_sorting_date[LANGUAGE_NONE][0]['value']= $record['field_sorting_date'];
+    $node->field_type_of_text[LANGUAGE_NONE][0] = array('tid' => $type_of_text_tid);
+
+    foreach(explode(';', $record['field_informea_tags']) as $i=>$term_name) {
+      $tid = ODataMigrationUtil::getTaxonomyTermByName(trim($term_name), $thesaurus_vid);
+      if (empty($tid)) {
+       throw new Exception('Cannot find type of term = ' . $record['field_informea_tags']);
+       }
+       $node->field_informea_tags[LANGUAGE_NONE][$i] = array('tid' => $tid);
+    }
+
+    $node->field_url['zh-hans'][0]['url']= $record['field_url_cn'];
+    $node->field_url['en'][0]['url'] = $record['field_url_en'];
     $node->type = $type;
     node_object_prepare($node);
     $node->language = $language;
@@ -62,7 +103,6 @@ class LegislationImportFromCSV {
     $node->promote = 0;
     $node->comment = 0;
     return $node;
-
   }
 
   /**
@@ -87,7 +127,7 @@ class LegislationImportFromCSV {
 
 $arguments = drush_get_arguments();
 if(empty($arguments[2]) || empty($arguments[3])) {
-  drush_set_error(sprintf('Usage: "drush scr %s /path/to/file.csv https://www.informea.org"', basename(__FILE__)));
+  drush_set_error(sprintf('Usage: "drush scr %s /informea/drush/Chinese.csv local.informea"', basename(__FILE__)));
   exit;
 }
 $runner = new LegislationImportFromCSV($arguments[2], $arguments[3]);
